7e625b8fbf06bf5f62dff5af73aba93f60d2d305
Christoph Wong <wong.christoph@gmail.com>
2014-06-20 17:24:13 +0800

    implemented turning for rover

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,4 +1,25 @@
 class Rover
+  attr_accessor :direction
+
   def initialize(args)
+    @direction = args[:direction]
+  end
+
+  def turn_left
+    self.direction = case direction
+    when 'N' then 'W'
+    when 'W' then 'S'
+    when 'S' then 'E'
+    when 'E' then 'N'
+    end
+  end
+
+  def turn_right
+    self.direction = case direction
+    when 'N' then 'E'
+    when 'W' then 'N'
+    when 'S' then 'W'
+    when 'E' then 'S'
+    end
   end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,2 +1,38 @@
 require 'spec_helper'
 
+describe Rover do
+  let(:rover) { Rover.new({ x: 1, y: 2, direction: "N", moves:"LMLMLMLMM" }) }
+
+  context 'when facing N' do
+    it '#turn_left' do
+      expect {rover.turn_left}.to change{ rover.direction }
+      .from('N')
+      .to('W')
+    end
+
+    it '#turn_right' do
+      expect {rover.turn_right}.to change{ rover.direction }
+      .from('N')
+      .to('E')
+    end
+  end
+
+  context 'when facing E' do
+    before { rover.direction = 'E' }
+    it '#turn_left' do
+      expect {rover.turn_left}.to change{ rover.direction }
+      .from('E')
+      .to('N')
+    end
+
+    it '#turn_right' do
+      expect {rover.turn_right}.to change{ rover.direction }
+      .from('E')
+      .to('S')
+    end
+  end
+
+  xit 'should report location' do
+    expect(rover.report_location).to eq("1 2 N")
+  end
+end
